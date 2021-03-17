3a127d736d8522b444d0c1d1629452c0050bd1f0
Smertin Nikita Job <smertin-nikita@mail.ru>
2021-03-17 15:17:54 +0500

    start bot

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-from random import randrange
-
-import vk_api
-from vk_api.longpoll import VkLongPoll, VkEventType
+import vk_bot
 
 import os
 from dotenv import load_dotenv
@@ -9,26 +6,9 @@ dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
 
-token = os.environ.get("TOKEN")
+token = os.environ.get("FULL_TOKEN")
+group_id = os.environ.get("GROUP_ID")
 
 
-vk = vk_api.VkApi(token=token)
-longpoll = VkLongPoll(vk)
-
-
-def write_msg(user_id, message):
-    vk.method('messages.send', {'user_id': user_id, 'message': message,  'random_id': randrange(10 ** 7)})
-
-
-for event in longpoll.listen():
-    if event.type == VkEventType.MESSAGE_NEW:
-
-        if event.to_me:
-            request = event.text
-
-            if request == "привет":
-                write_msg(event.user_id, f"Хай, {event.user_id}")
-            elif request == "пока":
-                write_msg(event.user_id, "Пока((")
-            else:
-                write_msg(event.user_id, "Не поняла вашего ответа...")
+vk_bot = vk_bot.Bot(token, group_id)
+vk_bot.start()
--- vk_bot.py
+++ vk_bot.py
@@ -1,3 +1,5 @@
+from random import randrange
+
 import vk_api.vk_api
 
 from vk_api.bot_longpoll import VkBotLongPoll
@@ -10,8 +12,18 @@ class Bot:
         self.vk = vk_api.VkApi(token=token)
         self.long_poll = VkBotLongPoll(self.vk, group_id)
         self.vk_api = self.vk.get_api()
-
+        self.event_listen = []
 
     def start(self):
         for event in self.long_poll.listen():
-            print(event)
+            # Пришло новое сообщение
+            if event.type == VkBotEventType.MESSAGE_NEW:
+                self.send_message(event.object.peer_id, f"{event.object.from_id}, я получил ваше сообщение!")
+
+    # def event_handler(self, event_type: VkBotEventType):
+    #     self.event_listen.append(event_type)
+    #     def decorator(func):
+    #         def inner(*args, **kwargs):
+
+    def send_message(self, peer_id, message):
+        self.vk.method('messages.send', {'user_id': peer_id, 'message': message,  'random_id': randrange(10 ** 7),})
