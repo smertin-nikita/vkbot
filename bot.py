802e36d518b3937112726c59adc9755b6388af98
Smertin Nikita Job <smertin-nikita@mail.ru>
2021-03-17 13:24:40 +0500

    Added class Bot

---
+++ bot.py
@@ -0,0 +1,12 @@
+import vk_api.vk_api
+
+from vk_api.bot_longpoll import VkBotLongPoll
+from vk_api.bot_longpoll import VkBotEventType
+
+
+class Bot:
+    def __init__(self, token, group_id):
+
+        self.vk = vk_api.VkApi(token=token)
+        self.long_poll = VkBotLongPoll(self.vk, group_id)
+        self.vk_api = self.vk.get_api()
