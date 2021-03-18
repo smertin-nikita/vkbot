4940245b133c4357ab8571bdf5fe13051918bf72
Smertin Nikita Job <smertin-nikita@mail.ru>
2021-03-18 14:19:26 +0500

    simple user dialog logic

--- vk_bot.py
+++ vk_bot.py
@@ -5,6 +5,8 @@ import vk_api.vk_api
 from vk_api.bot_longpoll import VkBotLongPoll
 from vk_api.bot_longpoll import VkBotEventType
 
+from vk_user import VkUser
+
 
 class BotMessageEvent:
     def __init__(self, event):
@@ -20,21 +22,42 @@ class Bot:
         self.vk = vk_api.VkApi(token=token)
         self.long_poll = VkBotLongPoll(self.vk, group_id)
         self.vk_api = self.vk.get_api()
+        self.users = {}
 
     def start(self):
         for event in self.long_poll.listen():
             # Пришло новое сообщение
             if event.type == VkBotEventType.MESSAGE_NEW:
                 event = BotMessageEvent(event)
-                print(event)
-                # vk_user = self.vk_api.users.get(self.get_peer_id(event))
-                self.send_message(event.peer_id, f"{event.peer_id}, я получил ваше сообщение!")
 
+                if event.from_id not in self.users:
+                    vk_user = self.add_user(event.from_id)
+                    self.send_message(event.peer_id, f"Привет {vk_user['first_name']} {vk_user['last_name']}")
+                else:# todo Реализовать юзер диалог. Затем инаплсулировать в класс
+                    vk_user = self.users[event.from_id]
 
-    # def event_handler(self, event_type: VkBotEventType):
-    #     self.event_listen.append(event_type)
-    #     def decorator(func):
-    #         def inner(*args, **kwargs):
+                    if not vk_user.get('bdate'):
+                        self.send_message(event.peer_id, f"Введите дата рождения (31.12.0000)!")
+
+                    if not vk_user.get('sex'):
+                        self.send_message(event.peer_id, f"Введите пол!")
+
+                    if not vk_user.get('city'):
+                        self.send_message(event.peer_id, f"Город!")
+
+                # todo Добавить клавиатуру стар поиска когда все данные будут готовы
+
+    def add_user(self, user_id):
+        # todo Использовать юзер класс или юзер объект
+        new_user = VkUser(user_id)
+        self.users[user_id] = new_user
+        return new_user
+
+    def get_vk_user_info(self, user_id):
+        return self.vk_api.users.get(
+            user_ids=user_id,
+            fields=['nickname', 'screen_name', 'first_name', 'last_name' 'sex', 'bdate', 'city', 'country']
+        )[0]
 
     def send_message(self, peer_id, message):
         self.vk_api.messages.send(
