5dba1bf2a2b66667041d096a58439ebcc42fd7d4
Smertin Nikita Job <smertin-nikita@mail.ru>
2021-03-22 15:21:20 +0500

    add simple ui

--- user_dialog.py
+++ user_dialog.py
@@ -1,5 +1,6 @@
 from enum import Enum
 
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 from vk_api.vk_api import VkApiMethod
 
 from vk_user import VkUser
@@ -13,13 +14,37 @@ class Command(Enum):
 class Mode(Enum):
     listen = ["Обычный режим"]
     get_answer = ["Режим ввода ответа"]
+    parse_age = False
 
 
 class Dialog:
 
     def __init__(self, vk_user: VkUser):
         self.user = vk_user
-        self.mode = Mode.listen
+
+        self.parse_age = False
+        self.parse_city = False
+        self.parse_sex = False
+
+        # Стартовая клавиатура
+        self.start_keyboard = VkKeyboard(one_time=False)
+        self.start_keyboard.add_button('Найти', color=VkKeyboardColor.POSITIVE)
+        self.start_keyboard.add_button('Настройки', color=VkKeyboardColor.SECONDARY)
+
+        # Настройки клавиатура
+        self.settings_keyboard = VkKeyboard(one_time=False)
+        self.settings_keyboard.add_button('Возраст', color=VkKeyboardColor.POSITIVE)
+        self.settings_keyboard.add_button('Город', color=VkKeyboardColor.POSITIVE)
+        self.settings_keyboard.add_button('Пол', color=VkKeyboardColor.POSITIVE)
+        self.settings_keyboard.add_button('Назад', color=VkKeyboardColor.SECONDARY)
+
+    def _start_keyboard(self):
+        self.keyboard = VkKeyboard(one_time=False, inline=False)
+        self.keyboard.add_callback_button(
+            label="Настройки",
+            color=VkKeyboardColor.PRIMARY,
+            payload={"type": "settings"},
+        )
 
     def _start_message(self):
         """
@@ -40,15 +65,43 @@ class Dialog:
                f"Добавить пол. Пример: /sex M\n" \
                f"Добавить город. Пример: /city Москва"
 
-    def input(self, message):
+    def input(self, message: str):
         """
         Функция принимающая сообщения пользователя
         :param message: Сообщение
         :return: Ответ пользователю, отправившему сообщение
         """
-        if self.mode == Mode.listen:
-            if message.startswith("/"):
-                for command in Command:
-                    if message[1::] in command.value:
-                        return "Help"
-            return self._start_message()
+        if message.lower() == 'настройки':
+            return {'message': 'Ваши критерии поиска', 'keyboard': self.settings_keyboard}
+
+        if message.lower() == 'назад':
+            return {'message': 'Готовы искать?', 'keyboard': self.start_keyboard}
+
+        if message.lower() == 'возраст':
+            self.parse_age = True
+            return {'message': 'Введите возраст', 'keyboard': self.settings_keyboard}
+
+        if message.lower() == 'город':
+            self.parse_city = True
+            return {'message': 'Введите город', 'keyboard': self.settings_keyboard}
+
+        if message.lower() == 'пол':
+            self.parse_sex = True
+            return {'message': 'Введите пол', 'keyboard': self.settings_keyboard}
+
+        if self.parse_age:
+            # parse age and user.age = age
+            self.parse_age = False
+            return {'message': 'Ваш возраст', 'keyboard': self.settings_keyboard}
+
+        if self.parse_city:
+            # parse city and user.city = city
+            self.parse_city = False
+            return {'message': 'Ваш город', 'keyboard': self.settings_keyboard}
+
+        if self.parse_sex:
+            # parse sex and user.sex = sex
+            self.parse_sex = False
+            return {'message': 'Ваш пол', 'keyboard': self.settings_keyboard}
+
+        return {'message': 'Привет', 'keyboard': self.start_keyboard}
--- vk_bot.py
+++ vk_bot.py
@@ -9,7 +9,7 @@ from user_dialog import Dialog
 from vk_user import VkUser
 
 
-class BotMessageEvent:
+class BotMessageEvent():
     def __init__(self, event):
         self.date = event.object['message']['date']
         self.from_id = event.object['message']['from_id']
@@ -17,43 +17,48 @@ class BotMessageEvent:
         self.text = event.object['message']['text']
 
 
+
 class Bot:
     def __init__(self, token, group_id):
 
-        self.vk = vk_api.VkApi(token=token)
+        self.vk = vk_api.VkApi(token=token, api_version='5.130')
         self.long_poll = VkBotLongPoll(self.vk, group_id)
         self.vk_api = self.vk.get_api()
         self.users = {}
-        self.create_keyboard()
 
-    def create_keyboard(self):
-        self.keyboard = VkKeyboard(one_time=False, inline=False)
-        self.keyboard.add_callback_button(
-            label="Помощь",
-            color=VkKeyboardColor.PRIMARY,
-            payload={"type": "help"},
-        )
+
+
+
+
 
     def start(self):
         for event in self.long_poll.listen():
             # Пришло новое сообщение
-            if event.type == VkBotEventType.MESSAGE_NEW:
+            if event.type == VkBotEventType.MESSAGE_NEW and event.obj.message["text"]:
+                print(event.obj.message['from_id'])
 
-                print(event.from_id)
-                # self.send_message(event.peer_id, self.users[event.from_id].input(event.text))
+                if event.obj.message['from_id'] not in self.users:
+                    self.users[event.obj.message['from_id']] = Dialog(event.obj.message['from_id'])
 
-            elif event.type == VkBotEventType.MESSAGE_EVENT:
-                if event.obj.from_id not in self.users:
-                    self.users[event.obj.from_id] = event.obj.from_id
+                self.send_message(event.obj.message['peer_id'], self.users[event.obj.message['from_id']].input(event.obj.message['text']))
 
-                print(event.obj.from_id)
-                if event.object.payload.get("type") == "help":
-                    self.send_message(event.obj.peer_id, 'help')
+            # elif event.type == VkBotEventType.MESSAGE_EVENT:
+            #     print('кнопка')
+            #     if event.object.payload.get("type") == "settings":
+            #         self.edit_message(event.obj.peer_id, 'ТЕСТ', event.obj.conversation_message_id, self.settings_keyboard)
 
-    def send_message(self, peer_id, message):
-        self.vk_api.messages.send(
+    def edit_message(self, peer_id, message, conversation_message_id, keyboard):
+        self.vk_api.messages.edit(
             peer_id=peer_id,
             message=message,
+            conversation_message_id=conversation_message_id,
+            keyboard=keyboard.get_keyboard()
+        )
+
+    def send_message(self, peer_id, answer):
+        self.vk_api.messages.send(
+            peer_id=peer_id,
+            message=answer['message'],
             random_id=get_random_id(),
-            keyboard=self.keyboard.get_keyboard()
+            keyboard=answer['keyboard'].get_keyboard()
         )
