6f8c7c140f96b68fa685c64b7876b539c877aa78
Smertin Nikita <smertin-nikita@mail.ru>
2021-03-31 23:27:10 +0500

    Init commit

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 import requests
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 
-from app import vk_bot
+import vkbot
 
 import os
 from dotenv import load_dotenv
 
 from app.vk_requester import VkRequester
-from app.vkinder import VKinder
 
 
 def get_url_for_token():
@@ -28,6 +25,8 @@ def get_url_for_token():
     exit(0)
 
 
+
+
 if __name__ == '__main__':
     # get_url_for_token()
 
@@ -44,19 +43,13 @@ if __name__ == '__main__':
     db_port = os.environ.get('DATABASE_PORT')
     db_name = os.environ.get('DATABASE_NAME')
 
-    bot = vk_bot.VkBot(community_token, group_id)
+    bot = vkbot.VkBot(community_token, group_id)
     requester = VkRequester(token=user_token)
 
-    engine = create_engine(
-        f'postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}'
-    )
-    # Base.metadata.create_all(engine)
-    session_factory = sessionmaker(bind=engine)
-    db_session = session_factory()
 
-    app = VKinder(bot, requester, db_session)
-    app.start()
-
+    @bot.message_handler(func=lambda m: True)
+    def echo_all(message):
+        bot.reply_to(message, message.text)
 
 
 
